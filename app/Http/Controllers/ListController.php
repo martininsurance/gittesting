935a82b96ce7d5b67806a287bf070d16fc1122d1
Vikas Sharma <vikas.sharma@websynergies.com>
2024-11-04 15:36:17 +0530

    list 2nd feature 1st commit

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -8,7 +8,12 @@ use App\Models\Lists;
 class ListController extends Controller
 {
     public function index(){
-        $list_data=Lists::get();
+        // this is the testing module for list 1st branch.
+        $list_data=Lists::orderBy('id','desc')->get();
         return view('lists/index',compact('list_data'));
+    }
+
+    public function create(){
+        return view('lists/create');
     }
 }
--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -1,1 +1,15 @@
-List Names
+List Names of List Module.
+This is for testing.
+!st List branch feature
+
+<table class="table" width="100%">
+    <thead>
+        <th>Name</th>
+        <th>Action</th>
+    </thead>
+
+    <tbody>
+        <td></td>
+        <td></td>
+    </tbody>
+</table>
