580a10271a735596a34583fcb19f9866346e8665
Vikas Sharma <vikas.sharma@websynergies.com>
2024-11-04 12:50:53 +0530

    1st commit note 2nd feature

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\Note;
 class NoteController extends Controller
 {
-    public function index(){
-        $notes=Note::get();
+    public function index()
+    {
+        $notes=Note::orderBy('id','desc')->get();
+        return view('note/index',compact('notes'));
     }
 }
