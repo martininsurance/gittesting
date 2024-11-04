bcf78199a4dfe480e0d82ef3239916ae4b069112
Vikas Sharma <vikas.sharma@websynergies.com>
2024-11-04 12:45:26 +0530

    1st commit note feature

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Note;
+class NoteController extends Controller
+{
+    public function index(){
+        $notes=Note::get();
+    }
+}
