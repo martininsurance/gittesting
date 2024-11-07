5876f14dbf9cd238be1e861ee85ea36eadbc8ccf
Vikas Sharma <vikas.sharma@websynergies.com>
2024-11-07 17:31:16 +0530

    dahboard feature 2nd commit

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,13 @@
 <h2>Dashboard</h2>
+
+<table>
+    <tr>
+        <td>Demo Count</td>
+        <td>{{ $demo->count() }}</td>
+    </tr>
+
+    <tr>
+        <td>Note Count</td>
+        <td>{{ $demo->count() }}</td>
+    </tr>
+</table>
